77ca6a024b46d4091082fb1855b289dba343daec
Yvonne Wang <yvonnewang22@gmail.com>
2015-07-15 09:05:26 -0400

    html done

--- trivia.js
+++ trivia.js
@@ -1,28 +1,15 @@
 $(document).ready(function(){
-//if user tries to submit an incomplete, alert will notify
-//create var with correct answer
-//compare the user's answer to correct answer
-//if correct, give user a point
-//if wrong, no point
-//at the end, give total score
+
   var score = 0;
   $(".score").text("Total score: " + score + "/10");
 
   $("input").on("click", function(event){
-    // event.preventDefault();
     alert($(this).val());
     if ($(this).val() == "correct"){
       score++
       $(".score").text("Total score: " + score + "/10");
-
     }
   });
-
-
-  // keeping track of points
-  //   if button with value of correct is selected, add 1 pt
-
-
 
   $("#submit").on("click", function(){
     alert("What are you submitting? lol")
